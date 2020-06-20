180c3295ed7a5a426155e8c982dffb1a7996d702
arnorhs <arnorhs@gmail.com>
2020-06-20 23:11:13 +0000

    update

--- joshnuss-svelte-copy-pasta/tailwind.config.js
+++ joshnuss-svelte-copy-pasta/tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  purge: ['./src/**/*.js', './src/**/*.svelte'],
+  // ...(process.env.NODE_ENV !== 'production'
+  //   ? {}
+  //   : { purge: ['./src/**/*.js', './src/**/*.svelte'] }),
   theme: {
     extend: {
       colors: {
         indigo: {
           lighter: '#b3bcf5',
           default: '#5c6ac4',
-          dark: '#202e78'
+          dark: '#ff0000'
         }
       }
     }
