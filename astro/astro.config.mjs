441cfadcf08190080560949c36f108da0ed7c8bb
arnorhs <arnorhs@gmail.com>
2023-01-26 23:06:36 +0000

    adding a netlify _redirects for broken links

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import allPosts from './src/gen/allPosts.json'
+import fs from 'fs/promises'
 
 export default defineConfig({
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
@@ -15,5 +17,17 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    {
+      name: 'redirects',
+      hooks: {
+        'astro:build:done': async ({ dir, routes }) => {
+          const str = allPosts
+            .map((post) => `/${post.permalink} /posts/${post.urlSlug} 301`)
+            .join('\n')
+          await fs.writeFile(`${dir.pathname}_redirects`, str, 'utf8')
+          console.log('Wrote redirects to _redirects')
+        },
+      },
+    },
   ],
 })
--- posts/src/lib/Collection.ts
+++ posts/src/lib/Collection.ts
@@ -11,7 +11,7 @@ export class Collection<T extends { permalink: string }> {
     return this.items.sort(sortBy('date', 'desc'))
   }
 
-  findItem(permalink: string): T | undefined {
+  findItem(permalink: string) {
     // use lodash to find by field name:
     return this.items.find((post) => post.permalink === permalink)
   }
