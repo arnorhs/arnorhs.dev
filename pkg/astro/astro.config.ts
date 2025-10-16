9e6bbb4574cc006779f464cb01f1ec81f1d8336a
arnorhs <arnorhs@gmail.com>
2025-10-16 18:34:20 +0000

    fix(astro): found the on-demand part in the docs

--- pkg/astro/astro.config.ts
+++ pkg/astro/astro.config.ts
@@ -4,12 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import cloudflare from '@astrojs/cloudflare'
 
-// https://astro.build/config
 export default defineConfig({
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   site: 'https://arnorhs.dev',
 
   integrations: [sitemap()],
