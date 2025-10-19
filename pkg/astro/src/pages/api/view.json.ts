6c324fc51f82a6c38d881ef081259685e1552bdf
arnorhs <arnorhs@gmail.com>
2025-10-19 03:18:17 +0000

    feat: color overhaul

--- pkg/astro/src/pages/api/view.json.ts
+++ pkg/astro/src/pages/api/view.json.ts
@@ -8,11 +8,13 @@ export async function POST({ request, locals }: APIContext) {
     const { path, referrer } = body
 
     // Write a data point to the Analytics Engine
-    locals.runtime.env.ARNORHS_ANALYTICS.writeDataPoint({
-      blobs: [path, referrer, request.headers.get('user-agent')], // Dimensions
-      doubles: [1], // Metric, e.g., a count of 1
-      indexes: [path], // For sampling/indexing
-    })
+    if (!import.meta.env.DEV) {
+      locals.runtime.env.ARNORHS_ANALYTICS.writeDataPoint({
+        blobs: [path, referrer, request.headers.get('user-agent')], // Dimensions
+        doubles: [1], // Metric, e.g., a count of 1
+        indexes: [path], // For sampling/indexing
+      })
+    }
   } catch (e) {
     console.error('Tracking Error:', e)
     return Response.json({ error: 'Error processing request' }, { status: 400 })
